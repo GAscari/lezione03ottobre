e75195c7d7649c0d6fcee3dfcceaf36916517d64
Giacomo Ascari <Giacomo.Ascari@marconi.loc>
2018-10-03 16:11:41 +0200

    Other stuff

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-var http = require('http');
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+
+app.use(function(req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
+    res.write("EHII");
+    res.end();
+});
+
+app.listen(3000, function() {
+    console.log("App started on port 3000");
+});
+/*var http = require('http');
 var conta=0;
 var server = http.createServer(function (request, response)
      {   conta+=1; 
@@ -9,4 +25,4 @@ var server = http.createServer(function (request, response)
     });
 
 server.listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://127.0.0.1:3000/');*/
